37aef38a6cae489b833c74fc8b9291664722e9ab
jingshi <chenyangwork1@163.com>
2022-07-04 17:35:28 +0800

    init

---
+++ src/test/java/com/example/hsm_demo/HsmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsm_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
